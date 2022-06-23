c18a51570e58329578ee10b499464584cbdd5aee
Aether Wu <aetherz13@gmail.com>
2022-06-23 18:09:42 +0800

    fish: Initial bringup
    * Environment variable check: Import environment variables if they're
      not correctly imported by checking ENVIRONMENT_D_IMPORTED from
      environment.d
    * Alias settings: Add some common-used parameters for alias to several
      commands
    * Keyring-unlock: Take password from user for unlocking keyring while
      using ssh without unlocked keyring
    * Set up Starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,21 @@
+# Import environment variables from environment.d if needed
+# https://github.com/systemd/systemd/issues/7641#issuecomment-680694017
+if not set -q ENVIRONMENT_D_IMPORTED
+    /usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator \
+        | string replace -r '([^=]*)=' 'set -gx $1 ' | source
+end
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    alias dg='git --git-dir=$HOME/src/dotfiles.git --work-tree=$HOME'
+    alias ls='ls --almost-all --classify --color=auto --group-directories-first -v'
+    alias ll='ls --all --human-readable -l --time-style=long-iso'
+    alias rg='rg -S'
+    alias vi='nvim'
+    # Unlock the login keyring
+    if not secret-tool lookup usage login > /dev/null
+       read --local --prompt-str 'login keyring password: ' --silent password
+       printf $password | gnome-keyring-daemon -r -d --unlock &> /dev/null
+    end
+
+    starship init fish | source
 end
